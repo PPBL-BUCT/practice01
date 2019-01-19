7cadc539b030f48174f1ea20be7ca1222224a9ae
lyc <a1379074822@163.com>
2019-01-19 11:33:31 +0800

    p1

--- lianyuechuan/SystemInfo.java
+++ lianyuechuan/SystemInfo.java
@@ -1,13 +1,6 @@
 public class SystemInfo {
     
 	public static void main(String[] args) {
-		File file = new File("myanswer.txt"); 
-    	file.createNewFile();
-    	FileWriter outFile = new FileWriter(file);   
-    	PrintWriter myFile = new PrintWriter(outFile);   
-    	System.getProperties().list(myFile);;
-    	resultFile.close();
-		
-		
+		System.getProperties().list(System.out);
 	}
 }
